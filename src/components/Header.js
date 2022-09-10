4bab4db85e8bf53693caf18f3fc5412c531b2a8e
arifpehlivan <arifpehlivanapap@gmail.com>
2022-09-10 03:06:15 +0300

    add toggle menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./header.css"
+    
+import {FaBars} from 'react-icons/fa';
+import {AiOutlineClose} from 'react-icons/ai';
+import {RiExchangeDollarLine} from 'react-icons/ri';
+import {GiPalmTree, GiShakingHands} from 'react-icons/gi';
+import {MdArrowForwardIos} from 'react-icons/md';
+import {BsBank, BsFillCreditCard2BackFill, BsCurrencyDollar} from 'react-icons/bs';
 
 const Header = () => {
+    const [show,setShow] = useState(false);
     return (
         <header>
+            <div className="toggle" onClick={()=> setShow(!show)}>
+                {show ? (<AiOutlineClose/>) : (<FaBars/>)}
+            </div>
             <a href='/#' className="logo">Bank</a>
-            <ul className="navigation">
-                <li><a href='/#'>Banking</a></li>
-                <li><a href='/#'>Credit Cards</a></li>
-                <li><a href='/#'>Loans</a></li>
-                <li><a href='/#'>Investing</a></li>
-                <li><a href='/#'>Insurance</a></li>
-                <li><a href='/#'>Retirement</a></li>
+            {/* <ul className="navigation"> */}
+            <ul className={show ? "active" : "navigation" }>
+                <li>
+                    <span><BsBank/></span>
+                    <a href='/#'>Banking</a>
+                    <i><MdArrowForwardIos/></i>
+                </li>
+                <li>
+                    <span><BsFillCreditCard2BackFill/></span>
+                    <a href='/#'>Credit Cards</a>
+                    <i><MdArrowForwardIos/></i>
+                </li>
+                <li>
+                    <span><BsCurrencyDollar/></span>
+                    <a href='/#'>Loans</a>
+                    <i><MdArrowForwardIos/></i>
+                </li>
+                <li>
+                    <span><RiExchangeDollarLine/></span>
+                    <a href='/#'>Investing</a>
+                    <i><MdArrowForwardIos/></i>
+                </li>  
+                <li>
+                    <span><GiShakingHands/></span>
+                    <a href='/#'>Insurance</a>
+                    <i><MdArrowForwardIos/></i>
+                </li>   
+                <li>
+                    <span><GiPalmTree/></span>
+                    <a href='/#'>Retirement</a>
+                    <i><MdArrowForwardIos/></i>
+                </li>
             </ul>
             <div className="login">
                 <button className="signIn">Sign In</button>
